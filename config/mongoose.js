dac5353e594e1bb1bb9e74672c2818f1c9487b4e
Piyush608090 <119125610+Piyush608090@users.noreply.github.com>
2024-04-25 23:42:11 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectToMongoose = async () =>{
-    mongoose.connect("mongodb://127.0.0.1:27017/employees")
+    mongoose.connect("mongodb+srv://piyush608090:piyush1@piyush.d2bp0tc.mongodb.net/?retryWrites=true&w=majority&appName=Piyush/employees")
     .then(() => console.log('Mongoose is connectecd'));
 }
 export default connectToMongoose;
